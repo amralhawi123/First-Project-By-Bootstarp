16c59d72e9dacb697a85e10d9822dccb02a51edb
amralhawi123 <amralhawi123>
2023-01-27 16:47:08 +0200

    git commit -m "Edit In Our-work Section"

--- js/amr.js
+++ js/amr.js
@@ -1,1 +1,28 @@
-console.log("amoor")
+let ClickLi = document.querySelectorAll(".our-work ul li")
+let RowElement = document.querySelectorAll(".our-work .row")
+
+ClickLi.forEach(element => {
+    element.onclick = function (el) {
+        for (let i = 0; i < RowElement.length; i++){
+            
+            if (element.dataset.row === RowElement[i].dataset.row) {
+                RowElement[i].classList.toggle("open")
+            } else {
+                RowElement[i].classList.remove("open")
+            }
+        }
+    }
+    element.addEventListener("click", (e) => {
+        
+        e.target.parentElement.querySelectorAll(".active").forEach(element => {
+
+            // Remove class active from all li
+            element.classList.remove("active")
+
+            // Add class active to all li
+            e.target.classList.add("active")
+            e.target.classList.add("rounded-pill")
+
+        })
+    })
+});
